99a06aaf8461f0475aa46d0a55c6dd26441d3a37
David <luis.saragay@epn.edu.ec>
2026-06-09 21:16:25 -0500

    Cambio de nombre de carpeta para arreglar error en Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Header from "./components/header/Header";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
-import Education from "./components/education/Education";
+import Education from "./components/estudios/Education";
+import Certificates from "./components/certificates/Certificates";
 import { Proyect } from "./components/project/Proyect";
 import {Contact} from "./components/contact/Contact";
 import { Footer } from "./components/footer/Footer";
@@ -14,6 +15,7 @@ function App() {
       <About/>
       <Skills/>
       <Education/>
+      <Certificates/>
       <Proyect/>
       <Contact/>
       <Footer/>
